5708ae434b4ec62e9a1fea83b859660e5787aa58
prashan24 <97212357+prashan24@users.noreply.github.com>
2022-03-06 20:38:49 +0530

    LC

---
+++ User/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def user_login(request):
+    return HttpResponse('<h1>User_Login<h1>')
+
+
+    # Create your views here.
